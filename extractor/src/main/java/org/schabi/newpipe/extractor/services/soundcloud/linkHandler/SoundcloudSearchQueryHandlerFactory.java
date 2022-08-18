32610fa2ddaed260322c65434dc4c45251b27981
evermind <evermind@tuxfamily.org>
2022-08-19 00:46:53 +0200

    searchfilters: convert Soundcloud to use new framework
    Available content filters:
    - All
    - Tracks
    - Users
    - Playlists
    
    Available sort filters:
    - 'Sort by'
    - Length
    - license

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/soundcloud/linkHandler/SoundcloudSearchQueryHandlerFactory.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/soundcloud/linkHandler/SoundcloudSearchQueryHandlerFactory.java
@@ -4,7 +4,9 @@ import org.schabi.newpipe.extractor.exceptions.ExtractionException;
 import org.schabi.newpipe.extractor.exceptions.ParsingException;
 import org.schabi.newpipe.extractor.exceptions.ReCaptchaException;
 import org.schabi.newpipe.extractor.linkhandler.SearchQueryHandlerFactory;
+import org.schabi.newpipe.extractor.search.filter.FilterItem;
 import org.schabi.newpipe.extractor.services.soundcloud.SoundcloudParsingHelper;
+import org.schabi.newpipe.extractor.services.soundcloud.search.filter.SoundcloudFilters;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -14,41 +16,30 @@ import java.util.List;
 import static org.schabi.newpipe.extractor.services.soundcloud.SoundcloudParsingHelper.SOUNDCLOUD_API_V2_URL;
 import static org.schabi.newpipe.extractor.utils.Utils.UTF_8;
 
-public class SoundcloudSearchQueryHandlerFactory extends SearchQueryHandlerFactory {
-
-    public static final String TRACKS = "tracks";
-    public static final String USERS = "users";
-    public static final String PLAYLISTS = "playlists";
-    public static final String ALL = "all";
+public final class SoundcloudSearchQueryHandlerFactory extends SearchQueryHandlerFactory {
 
     public static final int ITEMS_PER_PAGE = 10;
 
+    private SoundcloudSearchQueryHandlerFactory() {
+        super(new SoundcloudFilters());
+    }
+
     @Override
     public String getUrl(final String id,
-                         final List<String> contentFilter,
-                         final String sortFilter)
+                         final List<FilterItem> selectedContentFilter,
+                         final List<FilterItem> selectedSortFilter)
             throws ParsingException {
+
+        String url = SOUNDCLOUD_API_V2_URL + "search";
+        String sortQuery = "";
+
+        searchFilters.setSelectedContentFilter(selectedContentFilter);
+        searchFilters.setSelectedSortFilter(selectedSortFilter);
+        url += searchFilters.evaluateSelectedContentFilters();
+        sortQuery = searchFilters.evaluateSelectedSortFilters();
+
         try {
-            String url = SOUNDCLOUD_API_V2_URL + "search";
-
-            if (!contentFilter.isEmpty()) {
-                switch (contentFilter.get(0)) {
-                    case TRACKS:
-                        url += "/tracks";
-                        break;
-                    case USERS:
-                        url += "/users";
-                        break;
-                    case PLAYLISTS:
-                        url += "/playlists";
-                        break;
-                    case ALL:
-                    default:
-                        break;
-                }
-            }
-
-            return url + "?q=" + URLEncoder.encode(id, UTF_8) + "&client_id="
+            return url + "?q=" + URLEncoder.encode(id, UTF_8) + sortQuery + "&client_id="
                     + SoundcloudParsingHelper.clientId() + "&limit=" + ITEMS_PER_PAGE
                     + "&offset=0";
 
@@ -59,14 +50,5 @@ public class SoundcloudSearchQueryHandlerFactory extends SearchQueryHandlerFacto
         } catch (final IOException | ExtractionException e) {
             throw new ParsingException("Could not get client id", e);
         }
-    }
-
-    @Override
-    public String[] getAvailableContentFilter() {
-        return new String[]{
-                ALL,
-                TRACKS,
-                USERS,
-                PLAYLISTS};
     }
 }
