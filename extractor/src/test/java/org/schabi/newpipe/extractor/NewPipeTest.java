05fb62c485d6ee8dba187b3d645a40e63be7a915
ShareASmile <60492161+ShareASmile@users.noreply.github.com>
2023-06-16 23:55:08 +0530

    Revert remove-unused-methods

--- extractor/src/test/java/org/schabi/newpipe/extractor/NewPipeTest.java
+++ extractor/src/test/java/org/schabi/newpipe/extractor/NewPipeTest.java
@@ -18,8 +18,8 @@ public class NewPipeTest {
 
     @Test
     public void testAllServicesHaveDifferentId() throws Exception {
-        final HashSet<Integer> servicesId = new HashSet<>();
-        for (final StreamingService streamingService : NewPipe.getServices()) {
+        HashSet<Integer> servicesId = new HashSet<>();
+        for (StreamingService streamingService : NewPipe.getServices()) {
             final String errorMsg =
                     "There are services with the same id = " + streamingService.getServiceId()
                             + " (current service > " + streamingService.getServiceInfo().getName() + ")";
@@ -34,6 +34,11 @@ public class NewPipeTest {
     }
 
     @Test
+    public void getServiceWithName() throws Exception {
+        assertEquals(NewPipe.getService(YouTube.getServiceInfo().getName()), YouTube);
+    }
+
+    @Test
     public void getServiceWithUrl() throws Exception {
         assertEquals(getServiceByUrl("https://www.youtube.com/watch?v=_r6CgaFNAGg"), YouTube);
         assertEquals(getServiceByUrl("https://www.youtube.com/channel/UCi2bIyFtz-JdI-ou8kaqsqg"), YouTube);
@@ -42,5 +47,15 @@ public class NewPipeTest {
 
         assertEquals(getServiceByUrl("https://soundcloud.com/pegboardnerds"), SoundCloud);
         assertEquals(getServiceByUrl("https://www.google.com/url?sa=t&url=https%3A%2F%2Fsoundcloud.com%2Fciaoproduction&rct=j&q=&esrc=s&source=web&cd="), SoundCloud);
+    }
+
+    @Test
+    public void getIdWithServiceName() throws Exception {
+        assertEquals(NewPipe.getIdOfService(YouTube.getServiceInfo().getName()), YouTube.getServiceId());
+    }
+
+    @Test
+    public void getServiceNameWithId() throws Exception {
+        assertEquals(NewPipe.getNameOfService(YouTube.getServiceId()), YouTube.getServiceInfo().getName());
     }
 }
