e674b13b002ae594b93099dc15b697b6b5350b08
polymorphicshade <polymorphicshade@gmail.com>
2024-01-20 17:52:53 -0700

    added extractor components for SponsorBlock

--- extractor/src/main/java/org/schabi/newpipe/extractor/stream/StreamInfo.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/stream/StreamInfo.java
@@ -30,14 +30,20 @@ import org.schabi.newpipe.extractor.exceptions.ContentNotAvailableException;
 import org.schabi.newpipe.extractor.exceptions.ContentNotSupportedException;
 import org.schabi.newpipe.extractor.exceptions.ExtractionException;
 import org.schabi.newpipe.extractor.localization.DateWrapper;
+import org.schabi.newpipe.extractor.sponsorblock.SponsorBlockApiSettings;
+import org.schabi.newpipe.extractor.sponsorblock.SponsorBlockExtractorHelper;
+import org.schabi.newpipe.extractor.sponsorblock.SponsorBlockSegment;
 import org.schabi.newpipe.extractor.utils.ExtractorHelper;
 import org.schabi.newpipe.extractor.utils.ExtractorLogger;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import static org.schabi.newpipe.extractor.utils.Utils.isNullOrEmpty;
 
@@ -78,18 +84,24 @@ public class StreamInfo extends Info {
             + ']';
     }
 
-    public static StreamInfo getInfo(final String url) throws IOException, ExtractionException {
-        ExtractorLogger.d(TAG, "getInfo({url})", url);
-        return getInfo(NewPipe.getServiceByUrl(url), url);
+    public static StreamInfo getInfo(
+            final String url,
+            @Nullable final SponsorBlockApiSettings sponsorBlockApiSettings)
+            throws IOException, ExtractionException {
+        return getInfo(NewPipe.getServiceByUrl(url), url, sponsorBlockApiSettings);
     }
 
-    public static StreamInfo getInfo(@Nonnull final StreamingService service,
-                                     final String url) throws IOException, ExtractionException {
-        ExtractorLogger.d(TAG, "getInfo({service},{url})", service, url);
-        return getInfo(service.getStreamExtractor(url));
+    public static StreamInfo getInfo(
+            @Nonnull final StreamingService service,
+            final String url,
+            @Nullable final SponsorBlockApiSettings sponsorBlockApiSettings)
+            throws IOException, ExtractionException {
+        return getInfo(service.getStreamExtractor(url), sponsorBlockApiSettings);
     }
 
-    public static StreamInfo getInfo(@Nonnull final StreamExtractor extractor)
+    public static StreamInfo getInfo(
+            @Nonnull final StreamExtractor extractor,
+            @Nullable final SponsorBlockApiSettings sponsorBlockApiSettings)
             throws ExtractionException, IOException {
         ExtractorLogger.d(TAG, "getInfo({extractor})", extractor);
         extractor.fetchPage();
@@ -98,6 +110,13 @@ public class StreamInfo extends Info {
             streamInfo = extractImportantData(extractor);
             extractStreams(streamInfo, extractor);
             extractOptionalData(streamInfo, extractor);
+
+            if (sponsorBlockApiSettings != null) {
+                final SponsorBlockSegment[] sponsorBlockSegments =
+                        SponsorBlockExtractorHelper.getSegments(streamInfo, sponsorBlockApiSettings);
+                streamInfo.setSponsorBlockSegments(sponsorBlockSegments);
+            }
+
             return streamInfo;
 
         } catch (final ExtractionException e) {
@@ -409,6 +428,7 @@ public class StreamInfo extends Info {
     private boolean shortFormContent = false;
     @Nonnull
     private ContentAvailability contentAvailability = ContentAvailability.AVAILABLE;
+    private List<SponsorBlockSegment> sponsorBlockSegments = new ArrayList<>();
 
     /**
      * Preview frames, e.g. for the storyboard / seekbar thumbnail preview
@@ -763,5 +783,36 @@ public class StreamInfo extends Info {
 
     public void setContentAvailability(@Nonnull final ContentAvailability availability) {
         this.contentAvailability = availability;
+    }
+    
+    public SponsorBlockSegment[] getSponsorBlockSegments() {
+        return sponsorBlockSegments.toArray(new SponsorBlockSegment[0]);
+    }
+
+    public void setSponsorBlockSegments(final SponsorBlockSegment[] sponsorBlockSegments) {
+        this.sponsorBlockSegments.clear();
+        Collections.addAll(this.sponsorBlockSegments, sponsorBlockSegments);
+    }
+
+    public void addSponsorBlockSegment(final SponsorBlockSegment sponsorBlockSegment) {
+        sponsorBlockSegments.add(sponsorBlockSegment);
+    }
+
+    public void removeSponsorBlockSegment(final SponsorBlockSegment sponsorBlockSegment) {
+        sponsorBlockSegments.remove(sponsorBlockSegment);
+    }
+
+    public void removeSponsorBlockSegment(final String uuid) {
+        SponsorBlockSegment target = null;
+        for (final SponsorBlockSegment segment : sponsorBlockSegments) {
+            if (segment.uuid.equals(uuid)) {
+                target = segment;
+                break;
+            }
+        }
+
+        if (target != null) {
+            removeSponsorBlockSegment(target);
+        }
     }
 }
