3d673ee220b0c1f7da631fd00a2ae53a60c80455
Stypox <stypox@pm.me>
2025-07-28 23:13:23 +0200

    [YouTube] Add custom error for "Sign in to confirm ..."

---
+++ extractor/src/main/java/org/schabi/newpipe/extractor/exceptions/YoutubeSignInConfirmNotBotException.java
@@ -0,0 +1,7 @@
+package org.schabi.newpipe.extractor.exceptions;
+
+public class YoutubeSignInConfirmNotBotException extends ParsingException {
+    public YoutubeSignInConfirmNotBotException(final String message) {
+        super(message);
+    }
+}
