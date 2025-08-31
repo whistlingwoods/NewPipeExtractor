8261d3cc279139d209a9be8a52156a22a7bc4bab
Zsombor Gegesy <gzsombor@gmail.com>
2025-08-31 13:57:39 +0200

    Revert "Fix compile problems in JavaScript class"
    This reverts commit 470a7198619e12409a9b0c542227dc523c7d587b.

--- extractor/src/main/java/org/schabi/newpipe/extractor/utils/JavaScript.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/utils/JavaScript.java
@@ -10,25 +10,31 @@ public final class JavaScript {
     }
 
     public static void compileOrThrow(final String function) {
-        try (Context context = Context.enter()) {
-            context.setInterpretedMode(true);
+        try {
+            final Context context = Context.enter();
+            context.setOptimizationLevel(-1);
 
             // If it doesn't compile it throws an exception here
             context.compileString(function, null, 1, null);
+        } finally {
+            Context.exit();
         }
     }
 
     public static String run(final String function,
                              final String functionName,
                              final String... parameters) {
-        try (Context context = Context.enter()) {
-            context.setInterpretedMode(true);
+        try {
+            final Context context = Context.enter();
+            context.setOptimizationLevel(-1);
             final ScriptableObject scope = context.initSafeStandardObjects();
 
             context.evaluateString(scope, function, functionName, 1, null);
             final Function jsFunction = (Function) scope.get(functionName, scope);
             final Object result = jsFunction.call(context, scope, scope, parameters);
             return result.toString();
+        } finally {
+            Context.exit();
         }
     }
 
