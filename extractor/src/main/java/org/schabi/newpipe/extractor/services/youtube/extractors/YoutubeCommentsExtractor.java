bd15b86869ff7f0b3c80bda7c56a90c39f5a8ed3
h <hector19.rl@gmail.com>
2026-04-22 18:14:00 +0200

    Update YoutubeCommentsExtractor for live chat pages

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/extractors/YoutubeCommentsExtractor.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/extractors/YoutubeCommentsExtractor.java
@@ -203,6 +203,10 @@ public class YoutubeCommentsExtractor extends CommentsExtractor {
     public InfoItemsPage<CommentsInfoItem> getPage(final Page page)
             throws IOException, ExtractionException {
 
+        if (commentsDisabled && liveChatContinuation != null) {
+            return fetchLiveChat(page.getId());
+        }
+
         if (commentsDisabled) {
             return getInfoItemsPageForDisabledComments();
         }
@@ -490,6 +494,11 @@ public class YoutubeCommentsExtractor extends CommentsExtractor {
     @Override
     public boolean isCommentsDisabled() {
         return commentsDisabled && liveChatContinuation == null;
+    }
+
+    @Override
+    public boolean isLiveChat() {
+        return liveChatContinuation != null;
     }
 
     @Override
