d4d4c055e22022addff9266b9564bf7221f9881a
h <hector19.rl@gmail.com>
2026-04-23 18:55:00 +0200

    Fix fetchLiveChat using replay endpoint on fresh extractors

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/extractors/YoutubeCommentsExtractor.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/extractors/YoutubeCommentsExtractor.java
@@ -205,6 +205,7 @@ public class YoutubeCommentsExtractor extends CommentsExtractor {
 
         if ("live_chat".equals(page.getUrl())
                 || (commentsDisabled && liveChatContinuation != null)) {
+            isLiveStream = true;
             return fetchLiveChat(page.getId());
         }
 
@@ -411,6 +412,7 @@ public class YoutubeCommentsExtractor extends CommentsExtractor {
      */
     private InfoItemsPage<CommentsInfoItem> fetchLiveChat(final String chatContinuation)
             throws IOException, ExtractionException {
+        isLiveStream = true;
         final Localization localization = getExtractorLocalization();
         final byte[] json = JsonWriter.string(
                 prepareDesktopJsonBuilder(localization, getExtractorContentCountry())
